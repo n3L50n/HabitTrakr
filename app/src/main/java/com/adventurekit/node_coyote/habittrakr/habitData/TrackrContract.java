5ccc807f1d7a8f5b89271c9d4fe07c2b0696fd28
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-06 17:04:01 -0700

    Begin filling out helper class.

---
+++ app/src/main/java/com/adventurekit/node_coyote/habittrakr/habitData/TrackrContract.java
@@ -0,0 +1,23 @@
+package com.adventurekit.node_coyote.habittrakr.habitData;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by node_coyote on 4/6/17.
+ */
+
+public final class TrackrContract  {
+
+    // Empty constructor should not be used
+    private TrackrContract(){}
+
+    /**
+     * Inner class defines database constants for different habits
+     */
+    public static final class TrackrEntry implements BaseColumns {
+
+        // database name
+        public final static String TABLE_NAME = "";
+
+    }
+}
---
+++ app/src/main/java/com/adventurekit/node_coyote/habittrakr/habitData/TrakrDatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.adventurekit.node_coyote.habittrakr.habitData;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by node_coyote on 4/6/17.
+ */
+
+public class TrakrDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "";
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Use this constructor to create a new {@link TrakrDatabaseHelper}
+     * @param context get the context
+     */
+    public TrakrDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // onCreate builds a new database if one does not already exist
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    // Use to upgrade the database to a new version with the DATABASE_VERSION constant
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
