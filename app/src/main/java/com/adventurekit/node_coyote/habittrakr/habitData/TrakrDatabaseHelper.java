737f2ef4bc082b973d87bfcea74733166b951289
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-06 17:58:14 -0700

    Flesh out Database helper

--- app/src/main/java/com/adventurekit/node_coyote/habittrakr/habitData/TrakrDatabaseHelper.java
+++ app/src/main/java/com/adventurekit/node_coyote/habittrakr/habitData/TrakrDatabaseHelper.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.adventurekit.node_coyote.habittrakr.habitData.TrakrContract.TrakrEntry;
+
 /**
  * Created by node_coyote on 4/6/17.
  */
 
+/**
+ * Manages database creation and versioning for HabitTrakr app
+ */
 public class TrakrDatabaseHelper extends SQLiteOpenHelper {
 
     // Name of the actual database file
@@ -18,6 +23,7 @@ public class TrakrDatabaseHelper extends SQLiteOpenHelper {
 
     /**
      * Use this constructor to create a new {@link TrakrDatabaseHelper}
+     *
      * @param context get the context
      */
     public TrakrDatabaseHelper(Context context) {
@@ -27,12 +33,22 @@ public class TrakrDatabaseHelper extends SQLiteOpenHelper {
     // onCreate builds a new database if one does not already exist
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // SQL create table String
+        String SQL_CREATE_TRAKR_TABLE = "CREATE TABLE " + TrakrEntry.TABLE_NAME + " ("
+                + TrakrEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + TrakrEntry.COLUMN_JOURNAL + " INTEGER NOT NULL DEFAULT 0, "
+                + TrakrEntry.COLUMN_EXERCISE + " INTEGER NOT NULL DEFAULT 0. "
+                + TrakrEntry.COLUMN_GOALS + " TEXT, "
+                + TrakrEntry.COLUMN_KINDNESS + " TEXT);";
+
+        // Execute SQL string statement
+        db.execSQL(SQL_CREATE_TRAKR_TABLE);
 
     }
 
     // Use to upgrade the database to a new version with the DATABASE_VERSION constant
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        // We won't be upgrading yet, but maybe later
     }
 }
